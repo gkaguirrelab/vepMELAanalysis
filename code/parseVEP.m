482d2ce9fb9ad64e7a8feab0cc8f078b7fc55553
carlynpatterson <pattersonc@email.chop.edu>
2019-01-30 08:57:39 -0500

    code updates

--- code/parseVEP.m
+++ code/parseVEP.m
@@ -6,119 +6,209 @@
 % TTL pulse is VEP.response(1,:);
 % timebase is VEP.timebase
 
-% Bandpass filter for VEP signal
-Fs=VEP.params.frequencyInHz;
-lo=0.5; % low cut off frequency
-hi=500; % high cut off frequency
+% Variables
+lo=0.5; % low cut off frequency for bandpass filter
+hi=200; % high cut off frequency for bandpass filter
+dur_in_sec=1.75; % the length of the VEP epuch
+starttime=0.25; % if you want to start recording before (negative number) or after (positive number) the sync pulse units are in sec
 
-d=designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',lo,...
-    'HalfPowerFrequency2',hi,'SampleRate',Fs);
+% load compiled data for a single observer
+expID=input('experiment ID:','s');
+observerID=input('observer ID:','s');
 
-% % Get rid of 60hz powerline hum
-% d2=designfilt('bandstopiir','FilterOrder',2,'HalfPowerFrequency1',59,...
-%     'HalfPowerFrequency2',61,'SampleRate',Fs);
+filenameMAT=fullfile(['/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MELA_data/Experiments/OLApproach_VEP/'...
+         'Exp_' expID '/Subject_' observerID '/Exp' expID '_' observerID 'compiled.mat']);
+
+open(filenameMAT);
+VEP_main=ans.VEP;
+
+clear expID observerID ans
+
+% concatonate data across sessions
+TF_trials=[];
+VDS=[];
+VEP=[];
+
+for x=1:length(VEP_main)
     
-VEP_data=VEP.response(2,:);
-
-VEP_data=filter(d,VEP_data);
-
-% VEP_data=filter(d2,VEP_data);
-
-% VEP_data=VEP.response(2,:);
-
-clear lo hi
-
-% Find timestamp of TTL pulses
-TTL=VEP.response(1,:);
-D=0;
-timestamp=VEP.timebase;
-y=0;
-
-for x=2:length(TTL)
-    if TTL(1,x)>4 && TTL(1,(x-1))<4
-        y=y+1;
-        sync_pulse{y}=timestamp(1,x-D);
-        sync_loc{y}=x-D;
-        TTL_check{y}=TTL(1,x-D);
-    end
+    vds=VEP_main(x).VDS;
+    VDS=cat(1,VDS,vds);
+    
+    tf_trials=VEP_main(x).mtrp.TFtrials;
+    TF_trials=cat(1,TF_trials,tf_trials);
 end
 
-sync_pulse=cell2mat(sync_pulse);
-sync_loc=cell2mat(sync_loc);
-TTL_check=cell2mat(TTL_check);
+clear vds tf_trials
 
-clear x y
+for AA=1:length(VEP_main)
+    VEP=VEP_main(AA).VEP;
+    
 
-% parse VEP data
-dur_in_sec=3; % the length of the stimulus presentation
-dur_in_freq=dur_in_sec*Fs;
-repeat=7;
+    % Bandpass filter for VEP signal
+    Fs=VEP.params.frequencyInHz;
 
-for x=1:length(sync_loc)
-    parsed_VEP(x,:)=VEP_data(1,sync_loc(x):sync_loc(x)+dur_in_freq);
-end
+    d=designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',lo,...
+        'HalfPowerFrequency2',hi,'SampleRate',Fs);
+    
+    VEP_data=VEP.response(2,:);
 
-clear x
-
-XX=(1:length(parsed_VEP))/Fs;
-YY=mean(parsed_VEP,1);
-
-figure(1)
-plot(XX,YY,'-k')
-title('Average across frequencies');
-xlabel('Time(s)')
-ax=gca;
-ax.TickDir='out';
-ax.Box='off';
-ax.YLim=[-0.1 0.1];
-ax.XLim=[0 dur_in_sec];
-hold off
-% pause
-% 
-% for x=1:(size(parsed_VEP,1))
-%     plot(XX,parsed_VEP(x,:))
-%     hold off
-%     pause
-% end
-% 
-% clear x
-
-% parse data by frequency
-figure(2)
-
-A=unique(mtrp.TFtrials);
-VEP_Fr=zeros(length(A),repeat,length(parsed_VEP));
-xx=1;
-yy=1;
-t=(0:dur_in_freq-1)*1/Fs;       % Time vector
-f = Fs*(0:(dur_in_freq/2))/dur_in_freq; 
+    VEP_data=filter(d,VEP_data);
 
 
-for x=1:length(A)
-    for y=1:length(mtrp.TFtrials)
-        if A(x)==mtrp.TFtrials(y)
-           VEP_Fr(xx,yy,:)=parsed_VEP(y,:);
-            ft=fft(parsed_VEP(y,:));
-            P = abs(ft/dur_in_freq);
-            P_data(xx,yy,:) = P(1:dur_in_freq/2+1);
-           yy=yy+1;
+    % Find timestamp of TTL pulses
+    TTL=VEP.response(1,:);
+    timestamp=VEP.timebase;
+    y=0;
+    startFs=starttime*Fs;
+
+    for x=2:length(TTL)
+        if TTL(1,x)>4 && TTL(1,(x-1))<4
+            y=y+1;
+            sync_pulse(y)=timestamp(1,x+startFs);
+            sync_loc(y)=x+startFs;
         end
     end
-    t=num2str(A(x));
-    T=['frequency=' t];
+
+    % if the VEP recording started after the first sync pulse
+    if length(sync_pulse)==35
+        disp(['err sync pulse session:' num2str(AA)])
+        pause
+        sync_pulse=cat(2,timestamp(1,1),sync_pulse);
+        sync_loc=cat(2,1,sync_loc);
+    end
+
+    clear x y
+
+    % parse VEP data
+    dur_in_freq=dur_in_sec*Fs;
+
+    for x=1:length(sync_loc)
+        parsed_vep(x,:)=VEP_data(1,sync_loc(x):sync_loc(x)+dur_in_freq);
+    end
+
+    clear x
+
+    XX=(1:length(parsed_vep))/Fs;
+    YY=mean(parsed_vep,1);
     
-    subplot(1,2,1)
-    plot(XX,squeeze(mean(VEP_Fr(xx,:,:),2)))
-    title(T);
+    figure(1)
+    plot(XX,YY,'-k')
+    title('Average across frequencies');
     xlabel('Time(s)')
     ax=gca;
     ax.TickDir='out';
     ax.Box='off';
     ax.YLim=[-0.1 0.1];
     ax.XLim=[0 dur_in_sec];
+    hold off
+    pause
     
+%     for x=1:(size(parsed_vep,1))
+%         plot(XX,parsed_vep(x,:))
+%         hold off
+%         pause
+%     end
+    
+    parsed_VEP(AA,:,:)=parsed_vep;
+    
+    clear x sync_pulse sync_loc
+end
+
+clear AA x y ax D TTL timestamp parsed_vep VEP_main repeat
+
+
+% parse data by frequency
+figure(2)
+
+A=unique(TF_trials);
+yy=1;
+
+f = Fs*(0:(dur_in_freq/2))/dur_in_freq; 
+
+% creates 4D matrix where VEP_Fr is session, TF, repeat, time
+for w=1:size(parsed_VEP,1)
+    for x=1:length(A)
+        for y=1:length(TF_trials)
+            if A(x)==TF_trials(w,y)
+               VEP_Fr(w,x,yy,:)=parsed_VEP(w,y,:);
+               VDS_Fr(w,x,yy)=VDS(w,y);
+               
+               % Fourier transform
+                ft=fft(squeeze(parsed_VEP(w,y,:)));
+                P = abs(ft/dur_in_freq);
+                P_data(w,x,yy,:) = P(1:dur_in_freq/2+1);
+                
+                % select power spectra for each frequency
+                temp=find(round(A(x))==f);
+                P_dataFr(w,x,yy,:)=P_data(w,x,yy,temp);
+
+               yy=yy+1;
+            end
+        end
+        
+
+        subplot(1,2,1)
+        plot(XX,squeeze(squeeze(mean(VEP_Fr(w,x,:,:),3))))
+        title(['frequency=' num2str(A(x))]);
+        xlabel('Time(s)')
+        ax=gca;
+        ax.TickDir='out';
+        ax.Box='off';
+        ax.YLim=[-0.1 0.1];
+        ax.XLim=[0 dur_in_sec];
+
+        subplot(1,2,2)
+        plot(f,squeeze(squeeze(mean(P_data(w,x,:,:),3))),'-k')
+        ylabel('power spectra')
+        xlabel('frequency')
+        ax=gca;
+        ax.TickDir='out';
+        ax.Box='off';
+        ax.XLim=[0 130];
+        ax.YLim=[0 0.02];
+        pause
+        hold off
+
+        yy=1;
+    end
+end
+clear w yy x P ft
+
+temp=find(round(A(6))==f);
+Noise60=squeeze(P_data(:,:,:,temp));
+
+% Average data across repeats from all sessions
+vep_Fr=[];
+vds_Fr=[];
+p_dataFr=[];
+noise60=[];
+for yy=1:size(VEP_Fr,1)
+    vep_Fr=cat(2,vep_Fr,squeeze(VEP_Fr(yy,:,:,:)));
+    vds_Fr=cat(2,vds_Fr,squeeze(VDS_Fr(yy,:,:)));
+    p_dataFr=cat(2,p_dataFr,squeeze(P_dataFr(yy,:,:)));
+    noise60=cat(2,noise60,Noise60(yy,:));
+end
+VEP_FrM=squeeze(mean(vep_Fr,2));
+
+
+for xx=1:size(VEP_Fr,2)
+    ft=fft(VEP_FrM(xx,:));
+    P=abs(ft/dur_in_freq);
+    P2=P(1:dur_in_freq/2+1);
+
+    figure(3)
+    subplot(1,2,1)
+    plot(XX,VEP_FrM(xx,:),'-k')
+    title(['frequency=' num2str(A(xx))]);
+    xlabel('Time(s)')
+    ax=gca;
+    ax.TickDir='out';
+    ax.Box='off';
+    ax.YLim=[-0.1 0.1];
+    ax.XLim=[0 dur_in_sec];
+
     subplot(1,2,2)
-    plot(f,squeeze(mean(P_data(xx,:,:),2)),'-k')
+    plot(f,P2,'-k')
     ylabel('power spectra')
     xlabel('frequency')
     ax=gca;
@@ -128,10 +218,34 @@ for x=1:length(A)
     ax.YLim=[0 0.02];
     pause
     hold off
-    
-    yy=1;
-    xx=xx+1;
 end
 
 
-clear x P ft
+figure(4)
+% Plot mean Visual discomfort data
+subplot(2,1,1)
+p_dataFr=squeeze(p_dataFr);
+P_Fm=mean(p_dataFr,2);
+P_Fm2=mean(p_dataFr(6,:),2)-mean(noise60);
+P_Fstd=std(p_dataFr,[],2);
+errorbar(A,P_Fm,P_Fstd,'-ok')
+hold on
+errorbar(A(6),P_Fm2,P_Fstd(6),'-ob')
+ylabel('power spectra for stimulus frequency')
+xlabel('frequency')
+ax=gca;
+ax.TickDir='out';
+ax.Box='off';
+ax.XScale='log';
+
+% Plot mean Visual discomfort data
+subplot(2,1,2)
+VDSm=mean(vds_Fr,2);
+VDSstd=std(vds_Fr,[],2);
+errorbar(A,VDSm,VDSstd,'-ok')
+ylabel('visual discomfort scale')
+xlabel('frequency')
+ax=gca;
+ax.TickDir='out';
+ax.Box='off';
+ax.XScale='log';
--- code/runVEPanalysis.m
+++ code/runVEPanalysis.m
@@ -3,82 +3,119 @@
 % calls the experiment based on users input
 expID=input('experiment ID:','s');
 observerID=input('observer ID:','s');
-sessionID=input('session ID:','s');
+sessionID=input('session ID:');
 
-%% get metropsis data
+for x=1:length(sessionID)
 
-[mtrp]=parse_mtrp_xml(expID,observerID,sessionID);
-% %% Get VEP data
-%  filenameMAT=fullfile(['/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MELA_data/Experiments/OLApproach_VEP/'...
-%      'Exp_' expID '/Subject_' observerID '/Exp' expID '_' observerID sessionID '.mat']);
-%  
-%  VEP=open(filenameMAT);
-% 
-%  clear filenameMAT expID observerID sessionID
+    % Get VEP data
+     filenameMAT=fullfile(['/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MELA_data/Experiments/OLApproach_VEP/'...
+         'Exp_' expID '/Subject_' observerID '/Exp' expID '_' observerID num2str(sessionID(x)) '.mat']);
 
-%% Compare metropsis experiment start time to VEP start time to ensure the files are matched up properly
+      vep=open(filenameMAT);
+      
+  % Get MTRP data
+    [vep.mtrp]=parse_mtrp_xml(expID,observerID,num2str(sessionID(x)));
+    VEP(x)=vep;
+end
 
+    %% Get audio files, parse them, and record their numeric value
+    nTrials = 36; % number of trials in file
+    
+    for xx=1:length(VEP)
+        audioRec=VEP(xx).audioRec;
+        for tt = 1:nTrials
+            firstTrialIndex = ((tt - 1) * length(audioRec.data)/nTrials) + 1;
+            secondTrialIndex = ((tt) * length(audioRec.data)/nTrials) + 1;
+            trialAudio = audioRec.data(firstTrialIndex:secondTrialIndex);
+            [ firstTimePoint, secondTimePoint ] = grabRelevantAudioIndices(trialAudio, audioRec.Fs);
 
+            % index that starts the numerical rating
+            firstIndex = firstTimePoint*audioRec.Fs;
 
+            % index that ends the numerical rating
+            secondIndex = secondTimePoint*audioRec.Fs;
+            
+            if firstIndex<1
+                firstIndex=1;
+            end
+            
+            if isempty(firstIndex)
+                sound(trialAudio, audioRec.Fs);
+            else
+                sound(trialAudio(firstIndex:secondIndex), audioRec.Fs);
+            end
+            
+            vds=input('visual discomfort scale:');
+            
+            if isnan(vds)
+                sound(trialAudio, audioRec.Fs);
+                vds=input('visual discomfort scale:');
+            end
+            
+            VDS(tt)=vds;
+        end
+        VEP(xx).VDS=VDS;
+        clear VDS
+    end
+
+% save compiled data
+filenameComp=fullfile(['/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MELA_data/Experiments/OLApproach_VEP/'...
+         'Exp_' expID '/Subject_' observerID '/Exp' expID '_' observerID 'compiled.mat']);
+
+     save(filenameComp,'VEP')
 %% related functions
 function [mtrp]=parse_mtrp_xml(expID,observerID,sessionID)
     
-    % Load and read XML file
-    filenameXML=fullfile(['/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MTRP_data/Exp_' expID '/Subject_' observerID '/' observerID '_' sessionID '.dpkmx']); 
-    xml=fileread(filenameXML);
-    
+    % Load and read JSON file
     filenameJSON=fullfile(['/Users/melanopsin/Dropbox (Aguirre-Brainard Lab)/MTRP_data/Exp_' expID '/Subject_' observerID '/' observerID '_' sessionID '.lpkmx']); 
     json=fileread(filenameJSON);
-    
-    
+
+
     % Use json file to find the start time, observer ID, group, and session number of the experiment
     t=strfind(json,'Starting Time =')+16;
     s=strfind(json,'Session #')+9;
     o=strfind(json,'Subject:')+9;
     g=strfind(json,'Group:')+7;
-    
+
     mtrp.date=json(t:t+9);
     mtrp.starttime=json(t+11:t+18);
     mtrp.group=json(g:g+3);
     mtrp.observer=json(o:o+2);
-    
+
     if isempty(str2num(json(s+1)))
         mtrp.session=num2str(json(s));
     else
         mtrp.session=num2str(json(s:s+1));
     end
-  
-    
-    % find the temporal frequencies for each trial using XML file
- 
-    
-    k=strfind(xml,'[tf]</key>')+25; % 25 is the number of characters that the numeric value of TF is after the searched string
-    for x=1:length(xml)
+
+
+    % find the temporal frequencies
+
+    k=strfind(json,'tf]" =')+7; % 8 is the number of characters that the numeric value of TF is after the searched string
+    k=k(end-35:end);
+    for x=1:length(json)
         for y=1:length(k)
-            if x==k(y) && ~isempty(str2num(xml(k(y)+1)))
-                trial_TF(y)=str2num(xml(k(y):k(y)+1));
-            else if x==k(y) && isempty(str2num(xml(k(y)+1)))
-                   trial_TF(y)=str2num(xml(k(y))); 
+            if x==k(y) && ~isempty(str2num(json(k(y))))
+                trial_TF(y)=str2num(json(k(y):k(y)+1));
+            else if x==k(y) && isempty(str2num(json(k(y))))
+                   trial_TF(y)=str2num(json(k(y)+1:k(y)+3)); 
                 end
             end
         end
     end
     
-    mtrp.TFtrials=trial_TF;
-    
-    % find channel using XML file
-    S=strfind(xml,'[Scone]</key>')+24;
-    
-       
-    switch str2num(xml(S))
-        case -1
-            mrtp.channel=char('(L+M)-S');
-        case 0
-            mrtp.channel=char('L-M');
-        case 1
-            mtrp.channel=char('Lum');
+    for a=1:length(trial_TF)
+        if trial_TF(a)==3.2
+            trial_TF(a)=3.25;
+        end
+
+        if trial_TF(a)==1.6
+            trial_TF(a)=1.625;
+        end
     end
     
+    mtrp.TFtrials=trial_TF;
+
 end
 
 
