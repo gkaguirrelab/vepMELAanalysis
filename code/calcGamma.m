82ee582f87a9256d37a79f0ca3e658890c28b7b1
Carlyn Patterson Gentile <pattersonc@email.chop.edu>
2019-08-18 20:01:11 -0400

    updates for plotting and bug fix

--- code/calcGamma.m
+++ code/calcGamma.m
@@ -12,6 +12,14 @@ subjects=['MELA_0121';'MELA_0131';...
     'MELA_0209';'MELA_0204';...
     'MELA_0213';'MELA_0208'];
 
+% % observer ID for each subject to be compiled (MwA only)
+% subjects=[;'MELA_0131';'MELA_0175';...
+%     'MELA_0170';'MELA_0174';...
+%     'MELA_0194';'MELA_0179';...
+%     'MELA_0209';'MELA_0207';...
+%     'MELA_0213';'MELA_0208'];
+
+
 TemporalFrequency=[1.625 3.25 7.5 15 30];
 lb=50;
 ub=950;
@@ -52,18 +60,8 @@ for x=1:size(VEP_data,1)
             VEP(x,y,z,:)=vep;
 %             [psd(x,y,z,:),freqs]=pwelch(vep',window,[],[],Fs);
             [psd(x,y,z,:),freqs]=pmtm(vep',4,length(vep),Fs);
-            subplot(1,3,y)
-            hold on
-            plot(freqs,squeeze(squeeze(psd(x,y,z,:))),['-' C(y)],'LineWidth',L(z))
-            ax=gca;ax.XLim=[0 100];ax.Box='off';ax.TickDir='out';ax.YScale='log';ax.YLim=[0.000000001 0.001];
-            if z==1
-                 plot([30 30],[0.000000001 0.001],'--','Color',[0.5 0.5 0.5])
-                 plot([80 80],[0.000000001 0.001],'--','Color',[0.5 0.5 0.5])
-            end
         end
     end
-    pause
-    hold off
 end
 
 clear x y z
@@ -85,5 +83,12 @@ for x=1:size(psdM,1)
     end
 end
 
-[S,F,T]=spectrogram(squeeze(vepM(1,1,:)),100,98,128,Fs);
+figure(3)
+vep_plot=squeeze(vepM(1,3,:));
+subplot(1,2,1)
+plot((1:1:length(vep_plot))/Fs,vep_plot)
+ax=gca;ax.Box='off';ax.TickDir='out';
+subplot(1,2,2)
+[S,F,T]=spectrogram(vep_plot',[],[],[],Fs);
 helperCWTTimeFreqPlot(S,T,F,'surf','STFT for VEP signal','seconds','Hz')
+ax=gca;ax.YLim=[0 100];
