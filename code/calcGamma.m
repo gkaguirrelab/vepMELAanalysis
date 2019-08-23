bbb241b6aed02ca4f203496fe91cb0a9311bcdc5
Carlyn Patterson Gentile <pattersonc@email.chop.edu>
2019-08-23 06:57:45 -0400

    updates, code for prVEP

--- code/calcGamma.m
+++ code/calcGamma.m
@@ -46,30 +46,42 @@ for x=1:size(subjects,1)
         temp=find(strcmp(table2array(scoreTable(:,1)),observerID));
         temp2=scoreTable(temp,:);
         compiledData.subject=temp2;
+        VEP_data_trial(x,:,:,:,:)=compiledData.vep_Fr;
         VEP_data(x,:,:,:)=squeeze(nanmean(compiledData.vep_Fr,3));
 
 end
 
 clear ans compiledData observerID x filenameComp temp*
 
-figure(1)
 for x=1:size(VEP_data,1)
     for y=1:size(VEP_data,2)
         for z=1:size(VEP_data,3)
-            vep=squeeze(squeeze(VEP_data(x,y,z,:)));
+            vep=squeeze(squeeze(squeeze(VEP_data(x,y,z,:))));
             VEP(x,y,z,:)=vep;
-%             [psd(x,y,z,:),freqs]=pwelch(vep',window,[],[],Fs);
+    %             [psd(x,y,z,:),freqs]=pwelch(vep',window,[],[],Fs);
             [psd(x,y,z,:),freqs]=pmtm(vep',4,length(vep),Fs);
         end
     end
 end
 
-clear x y z
+
+for r=1:size(VEP_data_trial,1)
+    for x=1:size(VEP_data_trial,2)
+        for y=1:size(VEP_data_trial,3)
+            for z=1:size(VEP_data_trial,4)
+                vep=squeeze(squeeze(VEP_data_trial(r,x,y,z,:)))';
+                [S(r,x,y,z,:,:),F,T]=spectrogram(vep,[],[],[],Fs);
+            end
+        end
+    end
+end
+
+clear r x y z
 
 vepM=squeeze(nanmean(VEP,1));
 psdM=squeeze(nanmean(psd,1));
 
-figure(2)
+figure(1)
 for x=1:size(psdM,1)
     for y=1:size(psdM,2)
         subplot(1,3,x)
@@ -83,12 +95,11 @@ for x=1:size(psdM,1)
     end
 end
 
-figure(3)
-vep_plot=squeeze(vepM(1,3,:));
-subplot(1,2,1)
-plot((1:1:length(vep_plot))/Fs,vep_plot)
-ax=gca;ax.Box='off';ax.TickDir='out';
-subplot(1,2,2)
-[S,F,T]=spectrogram(vep_plot',[],[],[],Fs);
-helperCWTTimeFreqPlot(S,T,F,'surf','STFT for VEP signal','seconds','Hz')
-ax=gca;ax.YLim=[0 100];
+% figure(3)
+% vep_plot=squeeze(vepM(1,3,:));
+% subplot(1,2,1)
+% plot((1:1:length(vep_plot))/Fs,vep_plot)
+% ax=gca;ax.Box='off';ax.TickDir='out';
+% subplot(1,2,2)
+% helperCWTTimeFreqPlot(S,T,F,'surf','STFT for VEP signal','seconds','Hz')
+% ax=gca;ax.YLim=[0 100];
